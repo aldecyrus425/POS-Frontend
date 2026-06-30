a38e9c5afe46888e912bf89f8a53f5201c52427d
Cyrus Alde <admin@dolephilschool.com>
2026-06-30 15:18:43 +0800

    Purchase Order Platform Finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import CreateUser from "./components/forms/createUser"
 import CreateProduct from "./components/forms/createProduct"
 import CreateSupplier from "./components/forms/createSupplier"
 import CreateUnits from "./components/forms/createUnits"
+import CreatePurchaseOrder from "./components/forms/createPurchaseOrder"
 
 function App() {
   
-
   return (
     <Routes>
       <Route path="/" element={<Login />} />
@@ -15,6 +15,7 @@ function App() {
       <Route path="/create-product" element={<CreateProduct />} />
       <Route path="/create-supplier" element={<CreateSupplier />} />
       <Route path="/create-unit" element={<CreateUnits />} />
+      <Route path="/create-puchase-order" element={<CreatePurchaseOrder />} />
     </Routes>
   )
 }
