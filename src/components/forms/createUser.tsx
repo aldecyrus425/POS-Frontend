c27cb156015f8353a12fb401c48317f644b87ef0
Cyrus Alde <admin@dolephilschool.com>
2026-06-30 10:23:29 +0800

    Password input configuration finished

--- src/components/forms/createUser.tsx
+++ src/components/forms/createUser.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import CommonInputGroup from '../common/inputGroup'
 import SelectGroup from '../common/selectGroup'
+import PasswordInputGroup from '../common/passwordInputGroup'
 
 const CreateUser = () => {
   return (
-    <form action="" className='w-full h-full flex flex-col gap-2'>
+    <form action="" className='w-full h-full flex flex-col gap-2 p-2'>
       <CommonInputGroup label='First Name' />
       <CommonInputGroup label='Middle Name' />
       <CommonInputGroup label='Last Name' />
       <CommonInputGroup label='Username' />
-      {/* Password */}
-      {/* Confirm Password */}
+      <PasswordInputGroup label='Password' />
+      <PasswordInputGroup label='Confirm Password' />
       <CommonInputGroup label='Email' />
       <CommonInputGroup label='Contact Number' />
       <SelectGroup label='Role' options={[{label: "User", value: "User"}, {label: "Cashier", value: "Cashier"}]} />
