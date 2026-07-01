a6860ca65b03ca650eec757c8443c34bf094d60c
Cyrus Alde <admin@dolephilschool.com>
2026-07-01 15:30:29 +0800

    Purchase Order Table With Total

--- src/components/forms/createPurchaseOrder.tsx
+++ src/components/forms/createPurchaseOrder.tsx
@@ -1,126 +1,115 @@
 import React from 'react'
-import { BiMinus, BiPlus, BiSearch } from 'react-icons/bi'
-import GreenButton from '../common/greenButton'
+import { MdArrowRight } from 'react-icons/md'
 import CommonInputGroup from '../common/inputGroup'
+import { FiPlus, FiTrash, FiTrash2 } from 'react-icons/fi'
+import { PiBarcode, PiPlus } from 'react-icons/pi'
+import { LuPhilippinePeso } from 'react-icons/lu'
 
 const CreatePurchaseOrder = () => {
-    return (
-        <div className='w-screen h-screen'>
-            <div className='w-full h-full bg-slate-200 flex flex-col gap-2 relative'>
-                <div className='p-2'>
-                    <span className='font-medium text-lg'>Supplier Information</span>
-                    <div className="w-full h-10 flex gap-2">
-                        <div className="relative w-full">
-                            <div className="bg-white h-10 rounded-lg flex items-center gap-2 px-2">
-                                <BiSearch size={22} />
-                                <input
-                                    type="text"
-                                    placeholder="Please enter to search supplier"
-                                    className="w-full focus:outline-none font-medium"
-                                />
-                            </div>
-
-                            {/* <div className="absolute left-0 top-full mt-1 w-full bg-white rounded-lg shadow-lg z-10 font-medium text-slate-700 flex flex-col gap-2 p-2">
-                            <div className='cursor-pointer' onClick={() => console.log()}>
-                                Search Result
-                            </div>
-                            
-                        </div> */}
-                        </div>
-
-                        <div className="w-80">
-                            <GreenButton label="Add Supplier" />
-                        </div>
-                    </div>
-                    <div className='grid grid-cols-2 gap-x-3 gap-y-1'>
-                        <CommonInputGroup disabled label='Supplier Name' />
-                        <CommonInputGroup disabled label='Email' />
-                        <CommonInputGroup disabled label='Contact Number' />
-                        <CommonInputGroup disabled label='Address' />
-                    </div>
-                </div>
-                <hr className='border-slate-400 mt-4 p-2' />
-                <div className='p-2'>
-                    <span className='font-medium text-lg'>List of Products</span>
-                    <div className="w-full h-10 flex gap-2">
-                        <div className="relative w-full">
-                            <div className="bg-white h-10 rounded-lg flex items-center gap-2 px-2">
-                                <BiSearch size={22} />
-                                <input
-                                    type="text"
-                                    placeholder="Please enter to search products"
-                                    className="w-full focus:outline-none font-medium"
-                                />
-                            </div>
-
-                            {/* <div className="absolute left-0 top-full mt-1 w-full bg-white rounded-lg shadow-lg z-10 font-medium text-slate-700 flex flex-col gap-2 p-2">
-                            <div className='cursor-pointer' onClick={() => console.log()}>
-                                Search Result
-                            </div>
-                            
-                        </div> */}
-                        </div>
-
-                        <div className="w-80">
-                            <GreenButton label="Add Products" />
-                        </div>
-                    </div>
-                    <div className='flex gap-2 flex-col mt-2'>
-                        <div className='flex gap-2 w-full bg-slate-50 items-center rounded-md'>
-                            {/* Product Name */}
-                            <div className='w-full px-5 font-medium'>
-                                <span>Product 1</span>
-                            </div>
-                            {/* Actions */}
-                            <div className='w-100 flex gap-5 items-center justify-center h-10'>
-                                <span className='font-medium text-blue-500 w-40'>P 1,500</span>
-                                <div className='w-40 flex gap-5 items-center justify-center'>
-                                    <div className='bg-teal-200 rounded-full cursor-pointer'>
-                                        <BiPlus size={22} className='text-white' />
-                                    </div>
-                                    <span className='font-medium text-xl text-slate-700'>00</span>
-                                    <div className='bg-pink-500 rounded-full cursor-pointer'>
-                                        <BiMinus size={22} className='text-white' />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='flex gap-2 w-full bg-slate-50 items-center rounded-md'>
-                            {/* Product Name */}
-                            <div className='w-full px-5 font-medium'>
-                                <span>Product 1</span>
-                            </div>
-                            {/* Actions */}
-                            <div className='w-100 flex gap-5 items-center justify-center h-10'>
-                                <span className='font-medium text-blue-500 w-40'>P 1,500</span>
-                                <div className='w-40 flex gap-5 items-center justify-center'>
-                                    <div className='bg-teal-200 rounded-full cursor-pointer'>
-                                        <BiPlus size={22} className='text-white' />
-                                    </div>
-                                    <span className='font-medium text-xl text-slate-700'>00</span>
-                                    <div className='bg-pink-500 rounded-full cursor-pointer'>
-                                        <BiMinus size={22} className='text-white' />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='bg-slate-300 h-20 w-full absolute bottom-0'>
-                    <div className='w-full h-full flex justify-between items-center p-5'>
-                        <span className='font-semibold text-2xl'>TOTAL</span>
-                        <div className='w-100 flex justify-end items-center gap-3'>
-                            <span className='font-medium text-3xl text-blue-600'>00</span>
-                            <button className='w-30 py-2 rounded-2xl  font-semibold cursor-pointer border-amber-600 border text-amber-600 hover:bg-amber-600 hover:text-white'>
-                                Checkout
-                            </button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div className='w-full flex flex-col gap-3'>
+      {/* Title/Location */}
+      <div className='w-full flex justify-between'>
+        <span className='font-display font-medium text-xl text-slate-800'>Purchase Order</span>
+        <div className='flex items-center text-sm gap-2'>
+          <a href="purchase-order" className='text-indigo-600'>Purchase Orders</a> <MdArrowRight /> <a href="#">Create Purchase Order</a>
         </div>
-    )
+      </div>
+
+      {/* Purchase Order Information */}
+      <div className='bg-slate-50 w-full p-4 shadow rounded-lg border border-slate-300'>
+        <span className='font-medium text-indigo-600 text-lg'>Purchase Order Information</span>
+        <div className='grid grid-cols-3 gap-5 mt-2'>
+          <CommonInputGroup label='First Name' />
+          <CommonInputGroup label='First Name' />
+          <CommonInputGroup label='First Name' />
+          <CommonInputGroup label='First Name' />
+        </div>
+      </div>
+
+
+      {/* Purchase Order Item */}
+      <div className='bg-slate-50 w-full p-4 shadow rounded-lg border border-slate-300'>
+        <div className='flex justify-between'>
+          <span className='font-medium text-indigo-600 text-lg'>Purchase Order Items</span>
+          <div className='flex gap-3'>
+            <button className='cursor-pointer flex justify-center items-center w-40 gap-2 border border-indigo-600 text-indigo-600 rounded-lg py-1 font-semibold hover:bg-indigo-600 hover:text-white'><PiPlus /> Add Item</button>
+            <button className='cursor-pointer flex justify-center items-center w-40 gap-2 border border-indigo-600 text-indigo-600 rounded-lg py-1 font-semibold hover:bg-indigo-600 hover:text-white'><PiBarcode /> Scan Barcode</button>
+          </div>
+        </div>
+        <div className='p-2'>
+          <table className='border-y mt-2 w-full text-sm'>
+            <thead className='bg-slate-200 h-10'>
+              <tr>
+                <th className='items-center w-15'>#</th>
+                <th className='text-start w-70'>Product</th>
+                <th className='text-start w-70'>Description</th>
+                <th className='text-start w-20'>Unit</th>
+                <th className='text-start w-20'>Qty</th>
+                <th className='text-start w-50'>Unit Cost</th>
+                <th className='text-start w-30'>Discount (%)</th>
+                <th className='items-center'>Total</th>
+                <th className='items-center w-30'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className='h-10'>
+                <td className='text-center'>1</td>
+                <td>
+                  <input type="text" name="" id="" className='w-65 border border-slate-300 rounded-lg p-1' />
+                </td>
+                <td></td>
+                <td><input type="text" name="" id="" className='border border-slate-300 rounded-lg p-1 w-15 focus:outline-none' /></td>
+                <td><input type="text" name="" id="" className='border border-slate-300 rounded-lg p-1 w-15 focus:outline-none' /></td>
+                <td className='h-full'><div className='w-min border border-slate-300 flex items-center rounded-lg'><div className='h-full w-10 bg-slate-400 py-2 flex items-center justify-center rounded-l-lg'><LuPhilippinePeso /></div><input type="text" name="" id="" className='w-35 px-2 focus:outline-none' /></div></td>
+                <td><input type="text" name="" id="" className='w-25 border border-slate-300 rounded-lg p-1 focus:outline-none' /></td>
+                <td className='text-center'>P 5,000.00</td>
+                <td className='py-2 flex justify-center'>
+                  <button className='border rounded-md border-red-400 text-red-400 p-1 cursor-pointer hover:bg-red-400 hover:text-white'>
+                    <FiTrash2 size={15} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className='flex w-full justify-between mt-2'>
+            <button className='h-min flex gap-2 py-2 px-4 bg-blue-100 rounded-lg items-center font-medium text-sm text-indigo-500 cursor-pointer'>
+              <FiTrash2 />
+              Clear All
+            </button>
+
+            <div className='w-1/2 p-2 rounded-lg border border-slate-300 bg-slate-200 flex flex-col gap-3 text-sm font-medium'>
+              
+              <div className='w-full flex justify-between items-center'>
+                <span>Subtotal</span>
+                <span>P 9,650.00</span>
+              </div>
+
+              <div className='w-full flex justify-between items-center  '>
+                <span>Discount (Overall)</span>
+                <input type="text" className='border rounded-md bg-slate-50 border-slate-300 py-1 px-2'/>
+                <span>P 0.00</span>
+              </div>
+
+              <div className='w-full flex justify-between items-center'>
+                <span>Tax (12%)</span>
+                <span>P 1,000.00</span>
+              </div>
+
+              <hr />
+
+              <div className='w-full flex justify-between'>
+                <span className='text-lg'>Total Amount</span>
+                <span className='text-lg text-indigo-700'>P 10,808.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Buttons */}
+    </div>
+  )
 }
 
 export default CreatePurchaseOrder
